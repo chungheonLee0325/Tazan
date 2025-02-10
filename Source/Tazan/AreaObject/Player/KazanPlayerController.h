4e88e62fcb671bffe75da3c946bb0cc09d73bd3b
chungheonLee <fire6376@gmail.com>
2025-02-10 21:01:34 +0900

    Engine : GameInstance 적용 GameMode : Kazan Player, Player Controller 적용 Player : Enhanced Input Key 추가, 기본 애니메이션 추가, 스킬 구현중 Etc : Sample 코드 오류 메세지 수정

---
+++ Source/Tazan/AreaObject/Player/KazanPlayerController.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "KazanPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TAZAN_API AKazanPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
