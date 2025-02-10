c2c922f4fae80f69c5dd2bee7695e3d25734364f
choiyeontaek <dusxorchl@naver.com>
2025-02-10 19:40:13 +0900

    Sword Enemy : 작업 시작

---
+++ Source/Tazan/AreaObject/Monster/Variants/NormalMonsters/SwordEnemy/SwordEnemy.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Tazan/AreaObject/Monster/BaseMonster.h"
+#include "SwordEnemy.generated.h"
+
+UCLASS()
+class TAZAN_API ASwordEnemy : public ABaseMonster
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ASwordEnemy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	
+};
