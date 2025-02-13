6d48c24492d3298ec1b02177b3575aff97cd1d50
heenchon <heenchon@naver.com>
2025-02-13 10:10:08 +0900

    add: Yetuga State

---
+++ Source/Tazan/AreaObject/Monster/AI/Derived/AiMonster/Yetuga/Y_Anim.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Tazan/AreaObject/Monster/AI/Base/BaseAiState.h"
+#include "Y_Anim.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TAZAN_API UY_Anim : public UBaseAiState
+{
+	GENERATED_BODY()
+};
