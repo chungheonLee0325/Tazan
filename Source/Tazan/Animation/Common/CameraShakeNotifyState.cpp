e1728e3d483cc7a8b6f280fd43db8c7df59c7575
heenchon <heenchon@naver.com>
2025-02-19 13:44:21 +0900

    feat: 카메라 셰이크 애님 노티파이 (허접)

---
+++ Source/Tazan/Animation/Common/CameraShakeNotifyState.cpp
@@ -0,0 +1,30 @@
+﻿#include "CameraShakeNotifyState.h"
+#include "Kismet/GameplayStatics.h"
+#include "Camera/PlayerCameraManager.h"
+
+void UCameraShakeNotifyState::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration)
+{
+	if (!MeshComp || !MeshComp->GetWorld()) return;
+
+	APlayerController* PC = UGameplayStatics::GetPlayerController(MeshComp->GetWorld(), 0);
+	if (PC && CameraShakeClass)
+	{
+		APlayerCameraManager* CameraManager = PC->PlayerCameraManager;
+		UCameraShakeBase* ShakeInstance = CameraManager->StartCameraShake(CameraShakeClass, ShakeScale);
+		if (ShakeInstance)
+		{
+		}
+	}
+}
+
+void UCameraShakeNotifyState::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	if (!MeshComp || !MeshComp->GetWorld()) return;
+
+	APlayerController* PC = UGameplayStatics::GetPlayerController(MeshComp->GetWorld(), 0);
+	if (PC)
+	{
+		// Notify 종료 시, 동일한 셰이크 클래스로 실행중인 모든 인스턴스를 중지
+		PC->PlayerCameraManager->StopAllInstancesOfCameraShake(CameraShakeClass);
+	}
+}
