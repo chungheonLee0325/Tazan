30bbed142efe83ea458d2aabc4825c008f701e63
heenchon <heenchon@naver.com>
2025-02-22 17:14:24 +0900

    chore: AreaObject, 약점 히트 함수 const로 변경

--- Source/Tazan/AreaObject/Base/AreaObject.cpp
+++ Source/Tazan/AreaObject/Base/AreaObject.cpp
@@ -672,7 +672,7 @@ void AAreaObject::SetGuardState(bool bIsGuarding)
 	}
 }
 
-bool AAreaObject::IsWeakPointHit(FVector HitLoc)
+bool AAreaObject::IsWeakPointHit(const FVector& HitLoc)
 {
 	return false;
 }
