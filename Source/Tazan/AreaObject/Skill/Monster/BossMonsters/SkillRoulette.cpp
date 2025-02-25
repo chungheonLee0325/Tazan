3c5a186f8644d5e1af8dd37e60423c9fd3df1c45
heenchon <heenchon@naver.com>
2025-02-25 17:57:16 +0900

    feat: SkillRoulette, 거리 및 방향을 고려한 랜덤 스킬 1차

--- Source/Tazan/AreaObject/Skill/Monster/BossMonsters/SkillRoulette.cpp
+++ Source/Tazan/AreaObject/Skill/Monster/BossMonsters/SkillRoulette.cpp
@@ -3,24 +3,19 @@
 
 #include "SkillRoulette.h"
 
+#include "Tazan/AreaObject/Monster/BaseMonster.h"
 #include "Tazan/ResourceManager/KazanGameType.h"
 #include "Tazan/Utilities/LogMacro.h"
 
 
 USkillRoulette::USkillRoulette()
 {
-	PrimaryComponentTick.bCanEverTick = true;
 }
 
 void USkillRoulette::BeginPlay()
 {
 	Super::BeginPlay();
-}
-
-
-void USkillRoulette::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	Owner = Cast<ABaseMonster>(GetOwner());
 }
 
 void USkillRoulette::InitFromSkillBag(const FSkillBagData* SkillBagData)
@@ -29,17 +24,19 @@ void USkillRoulette::InitFromSkillBag(const FSkillBagData* SkillBagData)
 	AvailableSkillEntries.Empty();
 	if (SkillBagData)
 	{
-		// SkillBagData->TypeSkillBag는 TMap<EAiSkillType, FSkillBag>
+		// SKillBagData는 TypeSkillBag 정보를 가지고 있다.
+		// TypeSkillBag은 TMap<EAiSkillType, FSkillBag>
+		// EAiSkillType는 Short/Long/Weaveing 등
+		// FSkillBag은 TMap<int, float>. 각각 SkillID / Weight;
 		for (const auto& TypePair : SkillBagData->TypeSkillBag)
 		{
 			const FSkillBag& Bag = TypePair.Value;
-			// FSkillBag의 TMap<int, int>는 각각 <SkillID, Weight>
 			for (const auto& SkillPair : Bag.SkillID_Weight)
 			{
 				int SkillID = SkillPair.Key;
 				float Weight = static_cast<float>(SkillPair.Value);
-				SkillEntries.Add(FSkillRouletteEntry(SkillID, Weight));
-				AvailableSkillEntries.Add(FSkillRouletteEntry(SkillID, Weight));
+				SkillEntries.Add(FSkillRouletteEntry(TypePair.Key,SkillID, Weight));
+				AvailableSkillEntries.Add(FSkillRouletteEntry(TypePair.Key,SkillID, Weight));
 			}
 		}
 	}
@@ -47,8 +44,48 @@ void USkillRoulette::InitFromSkillBag(const FSkillBagData* SkillBagData)
 
 int USkillRoulette::GetRandomSkillID() const
 {
+	TArray<FSkillRouletteEntry> localEntries = AvailableSkillEntries;
+
+	float dist = Owner->GetDistToTarget();
+	FVector dir = Owner->GetDirToTarget();
+	dir.Normalize();
+
+	//직전 스킬 타입이 원거리 공격이면, 원거리 공격 확률 DOWN
+	if (PrevSkillType == EAiSkillType::Long)
+	{
+		// LOG_SCREEN("직전 스킬이 원거리 스킬입니다.");
+		ApplySkillWeight(localEntries,EAiSkillType::Long,0.3f);
+	}
+	// 거리가 멀면, 원거리 공격 확률 UP
+	if (dist > LongRange)
+	{
+		// LOG_SCREEN("원거리 공격 확률 UP");
+		ApplySkillWeight(localEntries,EAiSkillType::Long,3.0f);
+	}
+	// 거리가 짧으면, 위빙 스킬 확률 UP
+	else if (dist < ShortRange)
+	{
+		// LOG_SCREEN("위빙 확률 UP");
+		ApplySkillWeight(localEntries,EAiSkillType::Weaving,3.0f);
+
+		float forwardDot = FVector::DotProduct(dir,Owner->GetActorForwardVector());
+		float rightDot = FVector::DotProduct(dir,Owner->GetActorRightVector());
+		
+		if (forwardDot > 0.0f && rightDot >= 0.15f)
+		{
+			// LOG_SCREEN("우측 대각선!");
+			SetSkillWeight(localEntries,EAiSkillType::Right,7.0f);
+		}
+		if (forwardDot > 0.0f && rightDot <= 0.15f)
+		{
+			// LOG_SCREEN("좌측 대각선!");
+			SetSkillWeight(localEntries,EAiSkillType::Left,7.0f);
+		}
+	}
+	
+	
 	float totalWeight = 0.0f;
-	for (const FSkillRouletteEntry& Entry : AvailableSkillEntries)
+	for (const FSkillRouletteEntry& Entry : localEntries)
 	{
 		totalWeight += Entry.Weight;
 	}
@@ -60,12 +97,15 @@ int USkillRoulette::GetRandomSkillID() const
 
 	float RandomValue = FMath::FRandRange(0.f, totalWeight);
 	float AccumulatedWeight = 0.0f;
-	for (const FSkillRouletteEntry& Entry : AvailableSkillEntries)
+	for (const FSkillRouletteEntry& Entry : localEntries)
 	{
 		AccumulatedWeight += Entry.Weight;
 		if (RandomValue <= AccumulatedWeight)
 		{
 			LOG_PRINT(TEXT("스킬 ID: %d"),Entry.SkillID);
+
+			PrevSkillType = Entry.SkillType;
+			
 			return Entry.SkillID;
 		}
 	}
@@ -90,5 +130,39 @@ void USkillRoulette::AddSkillEntryByID(const int skillID)
 	if (skillEntry && !AvailableSkillEntries.Contains(*skillEntry))
 	{
 		AvailableSkillEntries.Add(*skillEntry);
+	}
+}
+
+void USkillRoulette::ApplySkillWeight(TArray<FSkillRouletteEntry>& entries, const EAiSkillType& skillType, const float ratio) const
+{
+	for (FSkillRouletteEntry& entry : entries)
+	{
+		if (entry.SkillType == skillType)
+		{
+			entry.Weight *= ratio;
+		}
+	}
+}
+
+void USkillRoulette::SetSkillWeight(TArray<FSkillRouletteEntry>& entries, const EAiSkillType& skillType,
+	const float weight) const
+{
+	for (FSkillRouletteEntry& entry : entries)
+	{
+		if (entry.SkillType == skillType)
+		{
+			entry.Weight = weight;
+		}
+	}
+}
+
+void USkillRoulette::SetSkillWeightByID(const int skillID, const float weight)
+{
+	for (FSkillRouletteEntry& entry : AvailableSkillEntries)
+	{
+		if (entry.SkillID == skillID)
+		{
+			entry.Weight = weight;
+		}
 	}
 }
