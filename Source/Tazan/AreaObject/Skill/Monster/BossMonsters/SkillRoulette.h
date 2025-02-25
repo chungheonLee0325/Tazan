9b6588ce58385f88abe5351602d0ed075b9a6fb3
heenchon <heenchon@naver.com>
2025-02-25 11:55:42 +0900

    feat: 스킬 가중치를 통한 랜덤 뽑기 및 쿨타임시 배열에서 제거 기능

---
+++ Source/Tazan/AreaObject/Skill/Monster/BossMonsters/SkillRoulette.h
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "SkillRoulette.generated.h"
+
+struct FSkillBagData;
+
+USTRUCT(BlueprintType)
+struct FSkillRouletteEntry
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SkillRoulette")
+	int SkillID;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SkillRoulette")
+	float Weight;
+
+	FORCEINLINE bool operator==(const FSkillRouletteEntry& Other) const
+	{
+		return SkillID == Other.SkillID;
+	}
+};
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class TAZAN_API USkillRoulette : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	USkillRoulette();
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	TArray<FSkillRouletteEntry> SkillEntries;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	TArray<FSkillRouletteEntry> AvailableSkillEntries;
+	
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+	
+	void InitFromSkillBag(const FSkillBagData* dt_SkillBag);
+	
+	UFUNCTION()
+	int GetRandomSkillID() const;
+
+	UFUNCTION()
+	void RemoveSkillEntryByID(const int skillID);
+	UFUNCTION()
+	void AddSkillEntryByID(const int skillID);
+};
