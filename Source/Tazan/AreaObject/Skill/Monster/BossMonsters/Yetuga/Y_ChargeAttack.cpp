924ce9ab41b75f5005fcde40990c7de5298b9e89
heenchon <heenchon@naver.com>
2025-02-27 03:02:22 +0900

    feat: 돌진 공격

--- Source/Tazan/AreaObject/Skill/Monster/BossMonsters/Yetuga/Y_ChargeAttack.cpp
+++ Source/Tazan/AreaObject/Skill/Monster/BossMonsters/Yetuga/Y_ChargeAttack.cpp
@@ -2,3 +2,31 @@
 
 
 #include "Y_ChargeAttack.h"
+
+#include "Tazan/AreaObject/Monster/Variants/BossMonsters/Yetuga/Yetuga.h"
+#include "Tazan/Utilities/LogMacro.h"
+
+UY_ChargeAttack::UY_ChargeAttack()
+{
+	MontageBlendTime = 0.3f;
+}
+
+void UY_ChargeAttack::OnCastStart(class AAreaObject* Caster, AAreaObject* Target)
+{
+	Super::OnCastStart(Caster, Target);
+	this->OnSkillCancel.BindUObject(this,&UY_ChargeAttack::Stun);
+}
+
+void UY_ChargeAttack::CancelCast()
+{
+	Super::CancelCast();
+}
+
+void UY_ChargeAttack::Stun()
+{
+	AYetuga* yetuga = Cast<AYetuga>(m_Caster);
+	if (yetuga)
+	{
+		yetuga->ChargeSkillStun();
+	}
+}
