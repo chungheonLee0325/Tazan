76f58a151760cd3dfd1977580a6781fd2427d0f3
heenchon <heenchon@naver.com>
2025-02-27 11:07:55 +0900

    feat: 예투가 돌

---
+++ Source/Tazan/AreaObject/Monster/Variants/BossMonsters/Yetuga/Yetuga_RockS.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Yetuga_RockS.h"
+
+#include "Components/SphereComponent.h"
+
+
+AYetuga_RockS::AYetuga_RockS()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USphereComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(RootComponent);
+}
+
+void AYetuga_RockS::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AYetuga_RockS::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
