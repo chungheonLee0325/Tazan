7be66d8a00694351e649296db19962009d16d41e
chungheonLee <fire6376@gmail.com>
2025-03-03 00:46:34 +0900

    BaseAIState : SuccessAIState, FailAIState 변수 / Setter 추가 : CheckState 추가

--- Source/Tazan/AreaObject/Monster/AI/Base/BaseAiState.h
+++ Source/Tazan/AreaObject/Monster/AI/Base/BaseAiState.h
@@ -19,28 +19,45 @@ class TAZAN_API UBaseAiState : public UObject
 	GENERATED_BODY()
 
 public:
+	// Create State - Base (Factory Pattern)
 	void SetAiFSM(UBaseAiFSM* AiFSM);
 	void SetAiKind(EAiStateType StateType);
-	EAiStateType AiStateType() const;
-	virtual void InitState() PURE_VIRTUAL(UBaseAiState::InitState);
 	void SetOwner(ABaseMonster* Owner);
+	virtual void InitState() PURE_VIRTUAL(UBaseAiState::InitState);
+
+	// Create State - Special Initialize (Builder Pattern)
+	virtual void CheckState() /*PURE_VIRTUAL(UBaseAiState::InitState)*/;
 	virtual void SetNextState(EAiStateType NextState);
+	void SetSuccessState(EAiStateType SuccessState);
+	void SetFailState(EAiStateType FailState);
+
+	// State 기본 동작
 	virtual void Enter() PURE_VIRTUAL(UBaseAiState::Enter,);
 	virtual void Execute(float dt) PURE_VIRTUAL(UAiState::Excute,);
 	virtual void Exit() PURE_VIRTUAL(UBaseAiState::Exit,);
-	
-	void AnimMontagePlay(ABaseMonster* baseMonster, UAnimMontage* montage);
-	virtual void AnimMontageEnd(UAnimMontage* Montage, bool bInterrupted);
+
+	// Getter
+	EAiStateType AiStateType() const;
+
+	// Interface
+	void ChangeState(EAiStateType NewState) const;
 
 protected:
 	EAiStateType m_AiStateType;
 
 	UPROPERTY()
-	UBaseAiFSM* m_AiFSM;	
+	UBaseAiFSM* m_AiFSM;
 
 	UPROPERTY()
 	ABaseMonster* m_Owner;
 
 	UPROPERTY()
+	// 기본 전이 State
 	EAiStateType m_NextState;
+	// 성공 후 전이 State
+	UPROPERTY()
+	EAiStateType m_SuccessState;
+	// 실패 후 전이 State
+	UPROPERTY()
+	EAiStateType m_FailState;
 };
