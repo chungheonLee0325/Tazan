671d15598fe4b8acfc30d2679a56dff3a4721b4a
heenchon <heenchon@naver.com>
2025-03-05 10:01:47 +0900

    feat: 예투가 돌던지기 노티파이스테이트

---
+++ Source/Tazan/Animation/Monster/Y_FastBallVisible.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Y_FastBallVisible.h"
+
+#include "Tazan/AreaObject/Monster/Variants/BossMonsters/Yetuga/Yetuga.h"
+
+void UY_FastBallVisible::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+                                     float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+}
+
+void UY_FastBallVisible::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+}
